ffad0f8e78c3e3386eabcf7073db80830ce262ba
Vinay Pandey <babbupandey@protonmail.com>
2021-03-10 23:34:29 -0800

    Create dotnet-desktop.yml Add xelatex executable Write error to console
    parent 2cb0e3b9788a224fb3a490f1b829f73344015d0b
    author Vinay Pandey <babbupandey@protonmail.com> 1615448069 -0800
    committer Vinay Pandey <vpandey@rms.com> 1615869142 -0700
    
    Create dotnet-desktop.yml
    Add xelatex executable
    Write error to console
    
    Create dotnet-desktop.yml
    Add xelatex executable
    Write error to console

--- efiling/libs/Generators.cs
+++ efiling/libs/Generators.cs
@@ -60,10 +60,11 @@ namespace efiling.libs {
         private static string compileTex(string texFileName, int compileCount) {
             var fullTexFilePath = Path.GetFullPath(texFileName);
             var outputFilePath = Path.GetFullPath(outputFileRelativePath);
+            var exeFilePath = Path.GetFullPath($"resources{Path.DirectorySeparatorChar}xelatex.exe");
 
             using var pProcess = new Process {
                 StartInfo = {
-                    FileName = "xelatex.exe",
+                    FileName = exeFilePath,
                     Arguments = "-file-line-error -interaction=nonstopmode -synctex=1 " +
                                 "-enable-installer -output-directory=out " + $@"""{fullTexFilePath}""",
                     UseShellExecute = false,
@@ -92,7 +93,7 @@ namespace efiling.libs {
                 if (exitCode == 0 || i < compileCount) continue;
 
                 Console.Error.WriteLine("Error creating pdf file");
-                // Console.Error.WriteLine(error);
+                Console.Error.WriteLine(error);
                 Console.WriteLine(output);
                 return "";
             }
